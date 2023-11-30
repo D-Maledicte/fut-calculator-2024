1e8e863d5d4b4680737c190dde94cba8839d7e54
Dmaledicte <matiasjesusmartel@outlook.es>
2023-11-30 16:59:48 -0300

    Primer commit

---
+++ assets/HelpPage.0957ee95.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.ce1525e0.js";import{_ as a,y as s,A as t,B as o,C as r,E as c}from"./index.6e5b2242.js";import"./render.fb2703df.js";const n=s({}),p=c("h5",{class:"q-mt-none"},"Help",-1);function _(f,i,l,d,m,u){return t(),o(e,{class:"q-pa-lg"},{default:r(()=>[p]),_:1})}var $=a(n,[["render",_]]);export{$ as default};
